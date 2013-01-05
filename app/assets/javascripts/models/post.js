d0d86e8b82510831fa6cecb694db87dbd746653f
Amiram Kehaty <amiramke@gmail.com>
2013-01-05 11:00:00 -0800

    cleanups

--- app/assets/javascripts/models/post.js
+++ app/assets/javascripts/models/post.js
@@ -1,7 +1,5 @@
-MessageBoard.Models.Post = Backbone.Model.extend({
-	
-	urlRoot: "/posts",
-  
+MessageBoard.Models.Post = Backbone.Model.extend({	
+	urlRoot: "/posts",  
   toJSON: function() {
     var json = _.clone(this.attributes);
     return { post: json };
--- app/assets/javascripts/views/posts_index.js
+++ app/assets/javascripts/views/posts_index.js
@@ -3,12 +3,10 @@ MessageBoard.Views.PostsIndex = Backbone.View.extend({
   initialize: function(){
     _.bindAll(this, "render");
     this.collection.bind("destroy", this.render);
-
   },
 
   render: function () {
-    var self = this;
-    
+    var self = this;    
 		this.$el.html(JST['posts/index']()); // Note that no collection is needed
                                          // to build the container markup.
     this.collection.each(function(post) {
